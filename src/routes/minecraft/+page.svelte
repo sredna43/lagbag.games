e03c57385d7cc4df4dc99d7c4797ceb7c06301c6
Anders Olson <sredna43@gmail.com>
2023-10-04 22:01:49 -0500

    add helper text and new links

--- src/routes/minecraft/+page.svelte
+++ src/routes/minecraft/+page.svelte
@@ -2,8 +2,21 @@
 
 <h1>Minecraft Servers</h1>
 <h4>ATM8 + Cobblemon Server (current)</h4>
-<a href="https://www.curseforge.com/minecraft/modpacks/all-the-mods-8/files/4749354">
+<a
+	href="https://www.curseforge.com/minecraft/modpacks/all-the-mods-8/files/4749354"
+	target="_blank"
+>
 	All The Mods 8 - v1.0.27</a
 >
 +
-<a href="/src/Cobblemon+ATM.zip" download>A few extras</a>
+<a
+	href="https://drive.google.com/file/d/1TToHnxA_427OjvSyvbrpDZhqVH-GaD3k/view?usp=sharing"
+	target="_blank">A few extras</a
+>
+<p>Don't worry, the links should open a new tab for you</p>
+
+<style>
+	p {
+		margin-top: 1rem;
+	}
+</style>
